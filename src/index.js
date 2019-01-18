a6539cb5cb181f51901db15da905fcfe3cfad26c
NYCSwan <megan.swanby@gmail.com>
2019-01-18 18:09:13 -0500

    Update html with g-captcha setup, import to index, change state for captcha and email pass to child form component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var myEnv = dotenv.config();
+dotenvExpand(myEnv);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
